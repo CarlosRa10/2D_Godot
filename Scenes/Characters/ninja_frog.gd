095ccf442a94afd13946c856a5f0bb5baa0df76f
CarlosRa10 <carloseduardoramirez10@gmail.com>
2024-12-02 01:02:07 -0400

    commit

--- Scenes/Characters/ninja_frog.gd
+++ Scenes/Characters/ninja_frog.gd
@@ -4,17 +4,28 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var appeared:bool = false
+var leaved_floor:bool = false
+var had_jump:bool = false
 
 func _ready():
 	$animaciones.play("appear")
 
 func _physics_process(delta):
 	# Add the gravity.
+	if is_on_floor():
+		#dejado el suelo
+		leaved_floor = false
+		#Ha Saltado
+		had_jump = false
 	if not is_on_floor():
+		#Si no estoy tocando suelo
+		if not leaved_floor:
+			$coyote_timer.start()
+			leaved_floor = true
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and right_to_jump():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -52,9 +63,25 @@ func decide_animation():
 		#Saltar
 		$animaciones.play("jump_up")
 		
-
-
+func right_to_jump():
+	#la primera vez had_jump = false, así que se pasa al siguiente chequeo
+	if had_jump: return false
+	if is_on_floor():
+		had_jump = true #modifica el estado de had_jump para decir que "se le permite saltar"
+		return true #retorna true para permitir saltar
+	elif not $coyote_timer.is_stopped():
+		had_jump = true
+		return true
+		
+##########
+#SEÑALES
+#########
 func _on_animaciones_animation_finished():
 	print($animaciones.animation)
 	if $animaciones.animation == "appear":
 		appeared = true
+
+
+func _on_coyote_timer_timeout():
+	
+	print("Booooom")

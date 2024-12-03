acdbe7746828df6a35db03f710923a607627af29
CarlosRa10 <carloseduardoramirez10@gmail.com>
2024-12-03 01:03:18 -0400

    commit

--- Scenes/Characters/ninja_frog.gd
+++ Scenes/Characters/ninja_frog.gd
@@ -8,9 +8,12 @@ var leaved_floor:bool = false
 var had_jump:bool = false
 var max_jumps:int = 2
 var count_jumps:int = 0
+var double_jump:bool = false
+var ray_cast_dimesion = 10.5
 
 func _ready():
 	$animaciones.play("appear")
+	$rayCast_wallJump.target_position.x = ray_cast_dimesion
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -31,8 +34,7 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and right_to_jump():
 		if count_jumps == 1:
-			allow_animation = false
-			$animaciones.play("double_jump")
+			double_jump = true
 		count_jumps += 1
 		velocity.y = JUMP_VELOCITY
 
@@ -46,20 +48,30 @@ func _physics_process(delta):
 	#print(velocity)
 	move_and_slide()
 	decide_animation()
+	#print($rayCast_wallJump.is_colliding())
+	print($rayCast_wallJump.get_collider())
 	
 
 func decide_animation():
-	if not allow_animation: return
+	if double_jump:
+		double_jump = false
+		allow_animation = false
+		#$animaciones.flip_h = velocity.x < 0 #hace que cuando caiga quede mirando a la derecha
+		$animaciones.play("double_jump")
+			
+	if not allow_animation: return #Si es falso, la función termina y no se ejecuta el resto del código dentro de esa función.
 	# EJE DE LAS X
 	if velocity.x == 0:
 		#idle
 		$animaciones.play("idle")
 	elif velocity.x < 0:
 		#izquierda
+		$rayCast_wallJump.target_position.x = -ray_cast_dimesion
 		$animaciones.flip_h = true
 		$animaciones.play("run")
 	elif velocity.x > 0:
 		#derecha
+		$rayCast_wallJump.target_position.x = ray_cast_dimesion
 		$animaciones.flip_h = false
 		$animaciones.play("run")
 	
